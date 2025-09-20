67d8f698f99c691a9a976c803ce95cf4259bf427
Rafael Ara Casanova <23028338@es.uveg.edu.mx>
2025-09-20 13:42:12 -0600

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import AppRouter from './router'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppRouter/>
+  </StrictMode>,
+)
