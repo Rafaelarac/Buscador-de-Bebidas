284b003ff002dd378e3756261ce4b2ea0da78c1d
Rafael Ara Casanova <23028338@es.uveg.edu.mx>
2025-09-21 16:26:13 -0600

    Añadieno formulario

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import { useMemo } from "react"
 import { NavLink,useLocation } from "react-router-dom"
 
 export default function Header() {
 
     const {pathname} = useLocation()
-    console.log(pathname);
+    
+    const isHome = useMemo(() => pathname === '/', [pathname])
+
 
     return (
         <header className="bg-slate-800">
@@ -22,10 +25,48 @@ export default function Header() {
                         >Inicio</NavLink>
                         <NavLink 
                             to="/favoritos"
-                            className="text-white font-bold uppercase"
+                            className={({isActive}) => isActive ? "text-orange-600 font-bold uppercase"  : "text-white font-bold uppercase"}
                         >Favoritos</NavLink> 
                     </nav>
                 </div>
+                {isHome && (
+                    <form 
+                        className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+                    >
+                        <div className="space-y-4">
+                            <label 
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes:</label>
+                            <input
+                                id="ingredient"
+                                type="text"
+                                name="ingredient"
+                                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Café"
+                            />
+                        </div>
+
+                        <div className="space-y-4">
+                            <label 
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoria:</label>
+                            <select
+                                id="ingredient"
+                                name="ingredient"
+                                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                            >
+                                <option value="">-- Seleccione --</option>
+                            </select>
+                        </div>
+                        <input 
+                            type="submit" 
+                            value="Buscar Recetas"
+                            className="cursor-pointer bg-orange-800 hover:bg-amber-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+                        />
+                    </form>
+                )}
             </div>
         </header>
     )
