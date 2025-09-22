1d1097d04ea0560388d5bbea359d0710702e0d6e
Rafael Ara Casanova <23028338@es.uveg.edu.mx>
2025-09-22 00:14:58 -0600

    Obtemer categorias

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,12 +1,17 @@
 import type { StateCreator } from "zustand"
+import { getCategories } from "../services/RecipeService"
 
 type Category = {}
 
 export type RecipisSliceType = {
-    categories: Category[]
+    categories: Category[],
+    fetchCategories: () => Promise<void>
 } 
 
 export const createRecipesSlice : StateCreator<RecipisSliceType> = ( ) => ({
     categories: [],
+    fetchCategories: async () => {
+        getCategories()
+    }
 })
 
