44b8bf9e645509ff653d46c19ba23f54571b2e3e
Rafael Ara Casanova <23028338@es.uveg.edu.mx>
2025-10-22 01:05:22 -0600

    Configurando slice Favoritos

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,6 +8,7 @@ export default function Modal() {
     const modal = useAppStore((state) => state.modal)
     const closeModal = useAppStore((state) => state.closeModal)
     const selectedRecipe = useAppStore((state) => state.selectedRecipe)
+    const handleFavoriteClick = useAppStore((state) => state.handleFavoriteClick)
 
     const renderIngredients = () => {
         const ingredients : JSX.Element[] = []
@@ -88,6 +89,7 @@ export default function Modal() {
                                         <button
                                             type='button'
                                             className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
+                                            onClick={() => handleFavoriteClick(selectedRecipe)}
                                         >
                                             Añadir a Favoritos
                                         </button>
--- src/stores/favoriteSlice.ts
+++ src/stores/favoriteSlice.ts
@@ -3,9 +3,21 @@ import type { Recipe } from "../types";
 
 export type FavoriteSliceType = {
     favorites: Recipe[]
+    handleFavoriteClick: (recipe: Recipe) => void
 }
 
-export const createFavoriteSlice : StateCreator<FavoriteSliceType> = () => ({
-    favorites: []
+export const createFavoriteSlice : StateCreator<FavoriteSliceType> = (set, get) => ({
+    favorites: [],
+    handleFavoriteClick: (recipe) => {
+        if (get().favorites.some( favorite => favorite.idDrink === recipe.idDrink)) {
+            set((state) => ({
+                favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink)
+            }))
+        } else {
+            set((state) => ({
+                favorites: [...state.favorites, recipe]
+            }))
+        }
+    }
 })
 
