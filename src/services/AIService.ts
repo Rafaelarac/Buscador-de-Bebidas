4714ac914fda14132189c402f8f17847253bd2ff
Rafael Ara Casanova <23028338@es.uveg.edu.mx>
2025-11-09 20:43:12 -0600

    Proyecto Finalizado

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -6,7 +6,9 @@ export default {
     async generateRecipe(prompt: string) {
         const result = streamText({
             model: openRouter('meta-llama/llama-3.3-70b-instruct:free'),
-            prompt
+            prompt,
+            system:`Eres un bartender que tiene 50 años de experiencia y le sirvio una bebida a james bond`,
+            temperature: 1
         })
         return result.textStream
     }
--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -5,13 +5,18 @@ import AIService from "../services/AIService";
 
 export type AISlice = {
     recipe: string
+    isGenerating: boolean
     generateRecipe: (prompt: string) => Promise<void>
 }
 
 export const createAISlice : StateCreator<AISlice, [], [], AISlice>= (set) => ({
     recipe: "",
+    isGenerating: false,
     generateRecipe: async (prompt) => {
-        
+        set({
+            recipe:"",
+            isGenerating: true
+        })
         const data = await AIService.generateRecipe(prompt)
 
         for await(const textPart of data) {
@@ -19,6 +24,9 @@ export const createAISlice : StateCreator<AISlice, [], [], AISlice>= (set) => ({
                 recipe: state.recipe + textPart
             }))
         }
+        set({
+            isGenerating: false
+        })
     }
 })
 
